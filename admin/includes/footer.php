edb66fc6402742a92bd2b1a19be5566518d0e5d8
creativecoder99 <anuragmohanty9999@gmail.com>
2024-05-07 19:31:37 +0530

    add files

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+  <strong>Copyright &copy; 2024 <a href="https://www.facebook.com/anurag.mohanty.391420?mibextid=ZbWKwL">Anurag Mohanty</a></strong>
+</footer>
